ee1136e812e0430b52a4804ac560b6a8f33378cd
NICO\vnico <vienat.nico@gmail.com>
2017-01-13 14:48:22 +0100

    Mise en place de la page login

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,7 +1,8 @@
 var router    = require('express').Router();
 var fs        = require('fs');
 
-router.get('/login', function(req, res) {
+router.get('/login', function (req, res) {
+    res.render('login.html', {});
 });
 
 router.get('/config', function(req, res) {
