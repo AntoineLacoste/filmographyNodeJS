c4c25bd87800dbe4838eb9bece28978e875c1b17
NICO\vnico <vienat.nico@gmail.com>
2017-01-15 19:22:22 +0100

    Gestion config

--- controllers/configController.js
+++ controllers/configController.js
@@ -1,12 +1,47 @@
 var router      = require('express').Router();
 var bodyParser  = require('body-parser');
 var db          = require('../config/db');
-var configModel = require('../model/configModel');
+var checkLogin = require('../middlewares/checkLogin');
+var Config = require('../model/configModel');
 
 var parser      = bodyParser.urlencoded({extended: false});
 
-router.get('/', checkLogin,  function(req, res) {
-    res.render('config.html', {});
+router.get('/', checkLogin, function (req, res) {
+    res.render('config.html', {salt: salt, perPage: perPage});
+});
+
+router.post('/', parser, function (req, res) {
+    
+    var newSalt = req.body.salt;
+    var newPerPage = req.body.perPage;
+
+    if(newSalt.length != 0) {
+        Config.find({ 'parameter': 'salt' }).then(function (configSalt) {
+            if(configSalt.length == 0) {             
+                Config({
+                    parameter: 'salt',
+                    value: newSalt
+                }).save(function (err, u) {});
+            } else {
+                configSalt.value.push(newSalt);
+            }
+        });
+    }
+    if(newPerPage.length != 0) {
+        Config.find({ 'parameter': 'perPage' }).then(function (configPerPage) {
+            if(configPerPage.length == 0) {             
+                Config({
+                    parameter: 'perPage',
+                    value: newPerPage
+                }).save(function (err, u) {});
+            } else {
+                configPerPage.value.push(newPerPage);
+            }
+        });
+    }
+   
+    res.redirect('/');
+    
 });
 
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -2,5 +2,6 @@ var router = require('express').Router();
 
 router.use('/admin', require('./adminController'));
 router.use('/', require('./movieController'));
+router.use('/config', require('./configController'));
 
 module.exports = router;
