d65b1e85528c1ecea1e5bf00b658f22e9c8364f1
NICO\vnico <vienat.nico@gmail.com>
2017-01-15 19:55:11 +0100

    Correction config

--- controllers/configController.js
+++ controllers/configController.js
@@ -7,7 +7,7 @@ var Config = require('../model/configModel');
 var parser      = bodyParser.urlencoded({extended: false});
 
 router.get('/', checkLogin, function (req, res) {
-    res.render('config.html', {salt: salt, perPage: perPage});
+    res.render('config.html', {perPage: perPage});
 });
 
 router.post('/', parser, function (req, res) {
@@ -16,35 +16,11 @@ router.post('/', parser, function (req, res) {
         value: req.body.perPage
     });
 
-    newPerPage.save();
-    
-    var newSalt = req.body.salt;
-    var newPerPage = req.body.perPage;
+    Config.find({ 'parameter': 'perPage' }).then(function (configPerPage) {
+        configSalt[0].value = newPerPage
+    });
 
-    if(newSalt.length != 0) {
-        Config.find({ 'parameter': 'salt' }).then(function (configSalt) {
-            if(configSalt.length == 0) {             
-                Config({
-                    parameter: 'salt',
-                    value: newSalt
-                }).save(function (err, u) {});
-            } else {
-                configSalt.value.push(newSalt);
-            }
-        });
-    }
-    if(newPerPage.length != 0) {
-        Config.find({ 'parameter': 'perPage' }).then(function (configPerPage) {
-            if(configPerPage.length == 0) {             
-                Config({
-                    parameter: 'perPage',
-                    value: newPerPage
-                }).save(function (err, u) {});
-            } else {
-                configPerPage.value.push(newPerPage);
-            }
-        });
-    }
+    newPerPage.save();
     res.redirect('/');
 });
 
